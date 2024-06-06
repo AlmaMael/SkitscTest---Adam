5b5b699d60328e7b1ce08487322c33a7593683ec
AlmaMael <adam.laflamme115@gmail.com>
2024-06-06 16:19:58 -0400

    Changement du Code Hashing
    Changement pour l'utilisation du script sur tout les utilisateurs

--- Api/hash_password.php
+++ Api/hash_password.php
@@ -1,18 +1,23 @@
 <?php
 include 'db.php';
 
+
 try {
-    $plain_password = 'testtest';
-    $hashed_password = password_hash($plain_password, PASSWORD_DEFAULT);
-    $courriel = 'testAdam@gmail.com';
+    $select_sql = "SELECT courriel, motpasse FROM utilisateur";
+    $select_stmt = $connexion->query($select_sql);
+    $utilisateurs = $select_stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $update_sql = "UPDATE utilisateur SET motpasse = :motpasse WHERE courriel = :courriel";
-    $update_stmt = $connexion->prepare($update_sql);
-    $update_stmt->bindParam(':motpasse', $hashed_password, PDO::PARAM_STR);
-    $update_stmt->bindParam(':courriel', $courriel, PDO::PARAM_STR);
-    $update_stmt->execute();
-
-    echo "Password for $courriel has been updated to a hashed password.";
+    foreach ($utilisateurs as $utilisateur) {
+        $hashed_password = password_hash($utilisateur['motpasse'], PASSWORD_DEFAULT);
+        
+        $update_sql = "UPDATE utilisateur SET motpasse = :motpasse WHERE courriel = :courriel";
+        $update_stmt = $connexion->prepare($update_sql);
+        $update_stmt->bindParam(':motpasse', $hashed_password, PDO::PARAM_STR);
+        $update_stmt->bindParam(':courriel', $utilisateur['courriel'], PDO::PARAM_STR);
+        $update_stmt->execute();
+        
+        echo "Le mot de passe pour {$utilisateur['courriel']} a été effectué.<br>";
+    }
 } catch (PDOException $e) {
     echo "Erreur: " . $e->getMessage();
 }
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # SkitscTest
- 
+ Téléchargé la base de donnée (skitscTest.sql) remis
+ Ajouter un compte manuellement donc Courriel, mot de passe, prenom, nom.
+
+ puis rendez-vous dans le domaine/api/hash_password.php pour hasher le mot de passe avant d'essayer de vous connecter.
+ Sinon, la connexion ne se fera jamais.
+
+Si les étapes sont suivis, le dashboard devrait s'ouvrir avec la présentation du nom choisi ainsi que le bouton pour fermer la session.
+
+
+
+P.S:
+Le bouton pour créer un compte n'est qu'un placeholder et la vérification d'erreur est à revoir. (Problème que je n'ai pas réussi à comprendre la cause)
+
